bed1fab8c5cb399104c6bb91b02142b9fec848e3
Heber <heberajbal2003@gmail.com>
2025-10-03 21:37:21 -0600

    fix
    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-baseURL: 'http://localhost:3000/',
+baseURL: 'ferreteria-api-production.up.railway.app',
 headers: { 'Content-Type': 'application/json' },
 });
 
